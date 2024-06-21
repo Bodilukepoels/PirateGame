69b34106d541046d1394f3ceb801b141249b3365
Bodilukepoels <112855865+Bodilukepoels@users.noreply.github.com>
2024-06-21 15:21:41 +0200

    deathAnimation + allerlei andere dingetjes

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
@@ -8,22 +9,60 @@ public class MenuController : MonoBehaviour
     public Rigidbody2D rb;
     public AudioSource playSound;
     public Animator animator;
+    public float currentHealth;
 
     Vector2 moveDirection;
+    bool isDead = false;
 
     void Update()
     {
+        if (isDead)
+        {
+            return;
+        }
+
         float moveX = Input.GetAxisRaw("Horizontal") * moveSpeed;
         float moveY = Input.GetAxisRaw("Vertical") * moveSpeed;
 
-       animator.SetFloat("Horizontal", moveDirection.x);
+        moveDirection = new Vector2(moveX, moveY).normalized;
+
+        animator.SetFloat("Horizontal", moveDirection.x);
         animator.SetFloat("Vertical", moveDirection.y);
         animator.SetFloat("Speed", moveDirection.sqrMagnitude);
-
-        moveDirection = new Vector2(moveX, moveY).normalized;
     }
+
     private void FixedUpdate()
     {
+        if (isDead)
+        {
+            rb.velocity = Vector2.zero;
+            return;
+        }
+
         rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+    }
+
+    public void TakeDamage(float damage)
+    {
+        currentHealth -= damage;
+
+        if (currentHealth <= 0 && !isDead)
+        {
+            isDead = true;
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        animator.SetBool("IsDead", true);
+        StartCoroutine(DieHandler());
+    }
+
+    IEnumerator DieHandler()
+    {
+        yield return new WaitForSeconds(5);
+        SceneManager.LoadScene("youDied");
+        Destroy(gameObject);
     }
 }
---
+++ Assets/Scripts/killPlayer.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class killPlayer : MonoBehaviour
+{
+    private MenuController menuController;
+
+    public string playerTag = "Player";
+
+    void Start()
+    {
+        Collider2D collider = GetComponent<Collider2D>();
+        if (collider != null)
+        {
+            collider.isTrigger = true;
+        }
+        else
+        {
+            Debug.LogError("Tilemap Collider 2D component not found on the GameObject.");
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag(playerTag))
+        {
+            menuController = other.GetComponent<MenuController>();
+
+            if (menuController != null)
+            {
+                menuController.TakeDamage(1000000);
+            }
+            else
+            {
+                Debug.LogError("MenuController component not found on the player GameObject.");
+            }
+        }
+    }
+}
